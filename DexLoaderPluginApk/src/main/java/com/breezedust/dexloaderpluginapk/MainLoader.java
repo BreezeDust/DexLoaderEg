85876d3b7101d407e669e40f8e956b3b518dab3e
BreezeDust <BreezeDust.com@gmail.com>
2016-04-06 14:42:31 +0800

    add simple load apk

---
+++ DexLoaderPluginApk/src/main/java/com/breezedust/dexloaderpluginapk/MainLoader.java
@@ -0,0 +1,13 @@
+package com.breezedust.dexloaderpluginapk;
+
+import com.baidu.dexloaderlib.BDClassPlugin;
+
+/**
+ * Created by BreezeDust on 16/4/6.
+ */
+public class MainLoader extends BDClassPlugin {
+    @Override
+    public String get() {
+        return "====apk";
+    }
+}
