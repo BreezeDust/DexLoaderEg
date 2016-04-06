00dc4ded19de232a064a36f7e6d26b3a33e95621
BreezeDust <BreezeDust.com@gmail.com>
2016-04-06 14:49:06 +0800

    add lib

---
+++ DexLoaderLib/src/main/java/com/baidu/dexloaderlib/BDClassPlugin.java
@@ -0,0 +1,11 @@
+package com.baidu.dexloaderlib;
+
+/**
+ * Created by BreezeDust on 16/4/6.
+ */
+public class BDClassPlugin implements BDPlugin {
+    @Override
+    public String get() {
+        return null;
+    }
+}
---
+++ DexLoaderLib/src/main/java/com/baidu/dexloaderlib/BDPlugin.java
@@ -0,0 +1,8 @@
+package com.baidu.dexloaderlib;
+
+/**
+ * Created by BreezeDust on 16/4/6.
+ */
+public interface BDPlugin {
+    String get();
+}
